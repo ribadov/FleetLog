484c3f1ff9d003c3ef02ba1d56d147137f30b53c
Rasim Ibadov <ribadov@student.ethz.ch>
2026-03-25 16:06:17 +0100

    fix(deploy): include prisma engine files in output tracing

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	outputFileTracingIncludes: {
+		"/*": [
+			"./node_modules/.prisma/client/**/*",
+			"./node_modules/@prisma/client/**/*",
+			"./prisma/dev.db",
+			"./prisma/prisma/dev.db",
+		],
+	},
 };
 
 export default nextConfig;
